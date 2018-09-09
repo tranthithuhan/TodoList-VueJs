f7198a8ec0337138e948dba3f427f9e5b64902be
Han <tranthithuhan@gmail.com>
2018-09-09 17:16:34 +0200

    Add todo list

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"/>
+    <create-todo v-on:add-todo="addTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+import CreateTodo from './components/CreateTodo'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+    CreateTodo
+  },
+  data () {
+    return {
+      todos: [{
+        id: 1,
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        id: 2,
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        id: 3,
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        id: 4,
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
+  },
+  methods: {
+    addTodo (title) {
+      this.todos.push({
+        title,
+        done: false
+      })
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="ui header medium centered">
+      Completed tasks:
+        {{
+        todos.filter(todo => {return todo.done}).length
+        }}
+    </div>
+    <div class="ui header medium centered">
+      Pending tasks:
+        {{
+        todos.filter(todo => {return !todo.done}).length
+        }}
+    </div>
+    <todo v-for="todo in todos"
+          v-bind:todo="todo"
+          v-bind:key="todo.id"
+          v-on:delete-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script type="text/javascript">
+import Todo from './Todo'
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo (todo) {
+      const index = this.todos.indexOf(todo)
+
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
